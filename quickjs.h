616aeeea286f1e718642d1f09e3f18708a63bd98
James <Icedshot@gmail.com>
2020-04-29 20:27:33 +0100

    oops

--- quickjs.h
+++ quickjs.h
@@ -918,7 +918,7 @@ int JS_SetModuleExportList(JSContext *ctx, JSModuleDef *m,
 //manually exported functions
 JSValueConst JS_GetActiveFunction(JSContext *ctx);
 JSValue js_proxy_constructor(JSContext *ctx, JSValueConst this_val,
-                                    int argc, JSValueConst *argv)
+                                    int argc, JSValueConst *argv);
 
 #undef js_unlikely
 #undef js_force_inline
